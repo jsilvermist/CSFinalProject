ec0bedcd71d13efa5492a8e8e6eb548bab8b314c
Jsilvermist <jsilvermist@silverlinkz.net>
2019-08-28 17:43:56 -0400

    Add smtp to send mail from contact page

--- FinalProject/Controllers/ContactController.cs
+++ FinalProject/Controllers/ContactController.cs
@@ -1,19 +1,81 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using FinalProject.Models;
 
 namespace FinalProject.Controllers
 {
     public class ContactController : Controller
     {
+        public ContactController() : base()
+        {
+            ViewData["ContactIsActive"] = "active";
+        }
+
         // GET: Contact
         public ActionResult Index()
         {
-            ViewData["ContactIsActive"] = "active";
-
             return View();
+        }
+
+        [HttpPost]
+        public string Index(ContactMesssage msg)
+        {
+            dynamic result = SendContactMessage(msg);
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            return serializer.Serialize(result);
+        }
+
+        private dynamic SendContactMessage(ContactMesssage msg)
+        {
+            // Command line argument must the the SMTP host.
+            SmtpClient client = new SmtpClient
+            {
+                Port = 26,
+                Host = "mail.jasonallen.dev",
+                EnableSsl = false,
+                Timeout = 10000,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential("csproject@jasonallen.dev", "KdF7[4?70k+T")
+            };
+
+            MailMessage mm = new MailMessage
+            {
+                From = new MailAddress("csproject@jasonallen.dev", msg.Name),
+                Sender = new MailAddress("csproject@jasonallen.dev"),
+                Subject = msg.Subject,
+                SubjectEncoding = Encoding.UTF8,
+                Body = msg.Message,
+                BodyEncoding = Encoding.UTF8,
+                IsBodyHtml = false,
+                DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure
+            };
+            mm.To.Add("contact@jasonallen.dev");
+            mm.ReplyToList.Add(new MailAddress(msg.Email, msg.Name));
+
+            try {
+                client.Send(mm);
+            } catch (Exception e) {
+                return new {
+                    success = false,
+                    error = e.Message
+                };
+            } finally {
+                mm.Dispose();
+                client.Dispose();
+            }
+
+            return new {
+                success = true,
+                error = (string)null
+            };
         }
     }
 }
